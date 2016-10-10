6d837b27bf3452d5017404ad2ebebc8f65f22c7b
gcmdeveloper <gcmtestuser@gmail.com>
2016-10-10 00:46:28 -0700

    'Updated'

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -3,13 +3,15 @@
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
+// var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-(function () {
+(function (window) {
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
 var byeSpeaker = {};
-byeSpeaker.name = names;
+
+// byeSpeaker.name = names;
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 var speakWord = "Good Bye";
@@ -17,13 +19,13 @@ var speakWord = "Good Bye";
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-byeSpeaker.speak = function (byeSpeaker.name) {
-  console.log(speakWord + " " + byeSpeaker.name);
+byeSpeaker.speak = function (names) {
+  console.log(speakWord + " " + names);
 }
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
-byeSpeaker.speak = byeSpeaker;
+window.byeSpeaker = byeSpeaker;
 
-}) ();
+}) (window);
